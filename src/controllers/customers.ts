3e02252b27291a459203f2971ac581c081e52dc2
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-04-19 15:01:19 +0500

    Отключение обязательного поля tg_phone
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/controllers/customers.ts
+++ src/controllers/customers.ts
@@ -5,13 +5,13 @@ import { generatePassword } from "../helpers/crypto";
 
 export const getCustomer = (api: WooCommerceRestApi) => {
   return (req: Request, res: Response) => {
-    if (!req.query.tg_phone) {
-      // Если параметр tg_phone не был передан
-      return res.status(400).json({ expected: "tg_phone" });
-    }
+    // if (!req.query.tg_phone) {
+    //   // Если параметр tg_phone не был передан
+    //   return res.status(400).json({ expected: "tg_phone" });
+    // }
     api
       .get(`customers`, {
-        tg_phone: req.query.tg_phone,
+        ...req.query,
         per_page: 1,
       })
       .then((response) => {
@@ -40,8 +40,6 @@ export const getCustomer = (api: WooCommerceRestApi) => {
 
 export const createCustomer = (api: WooCommerceRestApi) => {
   return (req: Request, res: Response) => {
-    console.log(req.query);
-
     const { tg_id } = req.body;
     const { email } = req.body;
     const { phone } = req.body;
@@ -55,16 +53,10 @@ export const createCustomer = (api: WooCommerceRestApi) => {
       })
       .then((response) => {
         // Successful request
-        console.log("Response Status:", response.status);
-        console.log("Response Headers:", response.headers);
-        console.log("Response Data:", response.data);
         return res.status(201).json(response.data);
       })
       .catch((error) => {
         // Invalid request, for 4xx and 5xx statuses
-        console.log("Response Status:", error.response.status);
-        console.log("Response Headers:", error.response.headers);
-        console.log("Response Data:", error.response.data);
         return res.status(500).json(error.response.data);
       })
       .finally(() => {
