46259e6fd4867f5a1f61c0450f9dbc801ff985d7
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-04-19 13:33:59 +0500

    Удаление unusing файлов
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/routes/establishmentsRouter.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response } from "express";
-
-const establishmentsRouter = express.Router();
-
-import { getEstablishments } from "../controllers/establishments";
-establishmentsRouter.get("/", getEstablishments);
-
-export default establishmentsRouter;
